4821ee02f2a4f9a3d6d66c4b81a3caf2181dc52f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-12 13:29:35 +0200

    Feature: new class Container with axios import and fetch method

---
+++ friends/src/Component/Container.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export default class Container extends React.Component {
+  state = {
+    friend: null,
+    errorMessage: '',
+    spinner: false,
+  }
+
+  fetchFriendWithAxios = () => {
+    // turn spinner on
+    this.setState({ spinner: true });
+
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        this.setState({ friend: response.data });
+      })
+      .catch(error => {
+        this.setState({ errorMessage: error.message });
+      })
+      .finally(() => {
+        this.setState({ spinner: false });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchFriendWithAxios();
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.errorMessage &&
+          <div className='error'>{this.state.errorMessage}</div>
+        }
+
+        {
+          this.state.spinner &&
+          <div className='loading'>Loading friends...</div>
+        }
+
+        {
+          this.state.friend &&
+          <div>{this.state.friend.name}</div>
+        }
+      </div>
+    );
+  }
+}
