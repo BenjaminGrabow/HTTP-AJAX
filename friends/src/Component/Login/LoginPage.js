78bd6c0b33877c80eb70f5744619d3fe7c759883
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-13 16:25:10 +0200

    Feature: added styled Component for Login Page

--- friends/src/Component/Login/LoginPage.js
+++ friends/src/Component/Login/LoginPage.js
@@ -1,4 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+width: 50%;
+margin: 2rem auto;
+background-color: black;
+border-radius: 50%;
+color: white;
+justify-content: center;
+align-items: center;
+height: 25rem;
+background: #c31432;
+background: -webkit-linear-gradient(to right,#240b36,#c31432);
+background: linear-gradient(to right,#240b36,#c31432);
+
+button {
+  margin-top: 2rem;
+background-color: red;
+border-radius: 50%;
+width: 15%;
+height: 5rem;
+box-shadow: 1rem .5rem .5rem black;
+}
+
+a {
+        text-decoration: none;
+        color: white;
+        font-size: 1.5rem;
+}
+`;
 
 class LoginPage extends React.Component {
         constructor(props) {
@@ -7,14 +39,14 @@ class LoginPage extends React.Component {
         }
         render() { 
                 return ( 
-                        <div className="loginBox">       
+                        <StyledDiv>       
                 <h1>Login Here</h1>
                 <p>Username</p>
                 <input onChange={this.theUsername} type="text" name="" placeholder="Enter Username" />
                 <p>Password</p>
                 <input onChange={this.thePassword} type="password" name="" placeholder="Enter Password" />
-                <a href="/friends">Login</a>
-                </div>
+                <button><a href="/friends">Login</a></button>
+                </StyledDiv>
                  );
         }
 }
